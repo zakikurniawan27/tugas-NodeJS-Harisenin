57ac82e61bb358ed3b89b14ec365a380f1d33793
zakikurniawan27 <kurniawan.zak279@gmail.com>
2024-05-14 00:00:45 +0800

    fix bug

--- src/config/config.js
+++ src/config/config.js
@@ -17,11 +17,12 @@ module.exports = {
   //   host: '127.0.0.1',
   //   dialect: 'mysql',
   // },
-  // production: {
-  //   username: 'root',
-  //   password: null,
-  //   database: 'database_production',
-  //   host: '127.0.0.1',
-  //   dialect: 'mysql',
-  // },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'mysql',
+  },
 };
