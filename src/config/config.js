85b7b73fb171284242d22e0bb3666c17d296eec5
zakikurniawan27 <kurniawan.zak279@gmail.com>
2024-05-14 19:22:40 +0800

    add firebase

--- src/config/config.js
+++ src/config/config.js
@@ -17,11 +17,11 @@ module.exports = {
   //   host: '127.0.0.1',
   //   dialect: 'mysql',
   // },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
+  // production: {
+  //   username: 'root',
+  //   password: null,
+  //   database: 'database_test',
+  //   host: '127.0.0.1',
+  //   dialect: 'mysql',
+  // },
 };
